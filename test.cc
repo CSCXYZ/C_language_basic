976b3e34ea647780b0f068e97515138d542c2dd8
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-04-13 17:55:37 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -31,3 +31,34 @@ int main()
 	printf("sum=%d\n", sum);
 	return 0;
 }  //1！+2！+3！...n!
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 17;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			printf("find it,subscript:%d\n", mid);
+			break;
+		}  
+	}
+	if (left > right)
+	{
+		printf("cannot find\n");
+	}
+	return 0;
+}  //find a number in the array(binary search)
