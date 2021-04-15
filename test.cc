9e0caf64a821bdf4541caa02dbc4fc2a5a8aacb6
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-04-15 11:44:48 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -160,3 +160,14 @@ int main()
 	printf("%d %d %d", a, b, c);
 	return 0;
 } // a b c依大到小排序
+
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("%d\n", i);
+	}
+	return 0;
+} //0~100 3的倍数
