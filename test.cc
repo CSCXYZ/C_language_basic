eb76d2abfb93ccc1f39275d6f492b8034c836084
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-04-16 09:17:23 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -247,3 +247,28 @@ int main()
 	printf("\ncount=%d\n", count);
 	return 0;
 } //质数 素数 PrimeNumber(Trial Division)
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i = 0;
+	int count = 0;
+	int j = 0;
+	for (i = 101; i <= 200; i+=2)
+	{
+		for (j = 2; j <= sqrt(i); j++)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+		}
+		if (j > sqrt(i))
+		{
+			count++;
+			printf("%d ", i);
+		}
+	}
+	printf("\ncount=%d\n", count);
+	return 0;
+}//质数 素数 PrimeNumber(Trial Division),若为素数一定含可除数小于等于原数开平方，若为素数一定非偶数
