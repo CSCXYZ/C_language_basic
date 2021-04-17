7c1ea19d0cec119ab73cafe92ef17a81e50b78e0
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-04-17 22:30:33 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -288,3 +288,17 @@ int main()
 	printf("count=%d\n", count);
 	return 0;
 }//编写程序数一下1到100的所有整数中出现多少个数字9
+
+int main()
+{
+	int i = 0;
+	double sum = 0.0;
+	int flag = 1;
+	for (i = 1; i <= 100; i++)
+	{
+		sum += flag*1.0 / i;
+		flag = -flag;
+	}
+	printf("%lf\n", sum);
+	return 0;
+}//1/1-1/2+1/3-1/4+1/5......+1/99-1/100
