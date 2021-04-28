ba388785a83e911eaae37423e47a77f844f092d3
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-04-28 20:53:53 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -498,3 +498,26 @@ int main()
 	}
 	return 0;
 }//判断素数的自定义函数，自除至平方根
+
+
+
+int is_leap_year(int y)
+{
+	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int year = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		//判断year是否为闰年
+		if (1 == is_leap_year(year))
+		{
+			printf("%d  ", year);
+		}
+	}
+	return 0;
+}//自定义函数判断是否为闰年
