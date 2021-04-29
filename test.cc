6bf17e5f77a8cc90757796696e77611af58016f3
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-04-29 16:25:11 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -521,3 +521,50 @@ int main()
 	}
 	return 0;
 }//自定义函数判断是否为闰年
+
+
+
+int binary_search(int arr[], int k,int sz)//二分查找自定义函数
+{
+
+	int left = 0;
+ 	int right = sz - 1;
+	while (left<=right)
+	{
+
+		int mid = (left + right) / 2;
+		if (arr[mid] < 7)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return  mid;
+		}
+	}
+	return -1;
+}
+
+int main()
+{
+	//二分查找
+	//在一个有序数组中查找具体的某个数
+	//如果找到了返回，这个数的下标。找不到的返回-1
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = binary_search(arr, k,sz);
+	if (ret == -1)
+	{
+		printf("can not find\n");
+	}
+	else
+	{
+		printf("find it the subscript is:%d\n", ret);
+	}
+	return 0;
+}
