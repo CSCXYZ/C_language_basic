42632eae759a69d1b44981f546d0b30c01c98e45
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-05-01 21:23:15 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -685,3 +685,19 @@ int main()
 	printf("%d\n", len);
 	return 0;
 }
+
+#include<string.h>
+int my_strlen(char* str)//递归的方法解决上述问题
+{
+	if (*str != '\n')
+		return 1 + my_strlen(str + 1);
+	else
+		return 0;
+}
+int main()
+{
+	char arr[] = "biteee";
+	int len = my_strlen(arr);//arr是数组，数组传参，传过去的不是整个数组，而是第一个元素的地址
+	printf("%d\n", len);
+	return 0;
+}
