8fff1e1dab597c50ad72bc5b4dbe1edcc5401323
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-05-02 16:38:18 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -816,3 +816,21 @@ int main()
 	}
 	return 0; 
 }
+
+
+
+int main()
+{
+	int arr[3][4] = { {1,2,3},{4,5} };//打印二维数组地址
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 4; j++)
+		{
+			printf("&arr[%d][%d]=%p ",i,j,&arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0; 
+}
