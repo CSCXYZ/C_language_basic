32b9e4f4046e8be862b35474ba83b2bf0bb3f38f
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-05-02 16:29:58 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -796,3 +796,23 @@ int main()
 	printf("%d\n", ret);
 	return 0;
 }
+
+
+
+int main()
+{
+	int arr[3][4] = { {1,2,3},{4,5} };//打印二维数组
+	//1 2 3 0
+	//4 5 0 0
+	//0 0 0 0
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		int j = 0;
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}printf("\n");
+	}
+	return 0; 
+}
