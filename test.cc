e8764d8625314b66fd17266d3eec30bfcb0ca56d
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-05-20 18:57:47 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -1743,3 +1743,26 @@ int main()
 	printf("ret=%d\n", ret);
 	return 0;
 }
+
+
+
+//n的K次方
+double Pow(int n, int k)
+{
+	//n^k=n*n^(k-1)
+	if (k < 0)
+		return (1.0 / (Pow(n, -k)));
+	else if (k == 0)
+		return 1;
+	else
+		return n * Pow(n, k - 1);
+}
+int main()
+{
+	int n = 0;
+	int k = 0;
+	scanf("%d%d", &n, &k);
+	double ret = Pow(n, k);
+	printf("ret=%lf\n", ret);
+	return 0;
+}
