56d743a4375690b5889b46c47892ae7d811503b7
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-05-22 09:54:03 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -1766,3 +1766,30 @@ int main()
 	printf("ret=%lf\n", ret);
 	return 0;
 }
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main()// 1！+2！+3！+...n!
+{
+	int i = 0;
+	int sum = 0;
+	int n = 0;
+	int j = 0;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+	{
+		int ret = 1;//循环中ret需要重置为1开始阶乘
+		for (j = 1; j <= i; j++)
+		{
+			ret *= j;
+		}
+		sum += ret;
+	}
+	printf("%d\n", sum);
+	system("pause");
+	return 0;
+}   
