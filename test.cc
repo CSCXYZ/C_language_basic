a95f9f00dbad6966ccd78f9b074add22d9b2c394
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-05-25 15:24:21 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -1897,3 +1897,29 @@ int main()
 	printf("%d\n", len);
 	return 0;
 }   
+
+
+
+
+int check_sys()
+{
+	int a = 1;
+	return (*(char*)&a);
+
+}
+int main()
+{
+	//写一段代码告诉我们当前机器的字节序是什么
+	//返回1，小端
+	//返回0，大端
+	int ret=check_sys();
+	if (ret == 1)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	return 0;
+}    
