cbb80d64332dd5431d298ed74bebeb0fb2a78eef
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-02 10:04:27 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2053,3 +2053,7 @@ void(*signal(int, void(*)(int)))(int);
 //signal是一个函数声明
 //signal函数的参数有2个，第一个是int.第二个是函数指针。该函数指针指向的函数的参数是int,返回类型是void
 //signal函数的返回类型也是一个函数指针：该函数指针指向的函数的参数是int,返回类型是void
+//简化
+typedef void(*pfun_t)(int);
+pfun_t signal(int, pfun_t);
+typedef unsigned int uint;
