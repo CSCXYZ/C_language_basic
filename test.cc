b519afb244ec32c26c7c55184f76b43a0af8d634
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-02 10:02:35 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2046,3 +2046,10 @@ int main()
 	(*p)("hello bit");
 	return 0;
 }
+
+
+
+void(*signal(int, void(*)(int)))(int);
+//signal是一个函数声明
+//signal函数的参数有2个，第一个是int.第二个是函数指针。该函数指针指向的函数的参数是int,返回类型是void
+//signal函数的返回类型也是一个函数指针：该函数指针指向的函数的参数是int,返回类型是void
