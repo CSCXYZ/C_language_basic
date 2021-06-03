ca3e4a7ee9da609e800329739b0f1e25d1ec64c2
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-03 11:13:35 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2057,3 +2057,60 @@ void(*signal(int, void(*)(int)))(int);
 typedef void(*pfun_t)(int);
 pfun_t signal(int, pfun_t);
 typedef unsigned int uint;
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void menu()
+{
+	printf("*********************\n");
+	printf("****1.add 2.sub******\n");
+	printf("****3.mul 4.div******\n");
+	printf("***    0.exit     ***\n");
+}
+int Add(int x, int y)
+{
+	return x + y;
+}
+int Sub(int x, int y)
+{
+	return x - y;
+}
+int Mul(int x, int y)
+{
+	return x * y;
+}
+int Div(int x, int y)
+{
+	return x / y;
+}
+int main()
+{
+	int input = 0;
+	int x = 0, y = 0;
+	//pfArr 是一个函数指针数组（函数指针的用途：转移表）
+	int(*pfArr[5])(int, int) = { 0,Add,Sub,Mul,Div };
+	do
+	{
+		menu();
+		printf("请选择：>");
+		scanf("%d", &input);
+		if(input>=1 && input<=4)
+		{
+		printf("请输入两个操作数：>");
+		scanf("%d%d", &x, &y);
+		int ret = pfArr[input](x, y);
+		printf("%d\n", ret);
+		}
+		else if (input == 0)
+		{
+			printf("退出\n");
+		} 
+		else
+		{
+			printf("选择错误\n");
+		}
+	} while (input);
+}
