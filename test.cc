28ecb99515de0e3af5f87683f56b0acde731765e
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-06 15:23:35 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2126,3 +2126,31 @@ int main()
 	int(*(*ppfArr)[4])(int, int) = &pfArr;
 	//ppfArr是一个指向 函数指针数组 的指针
 }
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int cmp_int(const void* e1, const void* e2)
+{
+	//比较两个整型值
+	return (*(int*)e1 - *(int*)e2);
+}
+void test1()
+{
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr, sz, sizeof(arr[0]), cmp_int);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
+int main()
+{
+	test1();
+	return 0;
+}
