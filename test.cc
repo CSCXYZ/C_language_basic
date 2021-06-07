2fe0bb609276c6fcc329274bd2c9462a8feadb41
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-07 15:04:16 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2157,3 +2157,51 @@ int main()
 	test1();
 	return 0;
 }
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//使用回调函数，模拟实现qsort采用冒泡的方式
+int cmp(const void* p1, const void* p2)
+{
+	return (*(int*)p1 - *(int*)p2);
+}
+void swap(void* p1, void* p2, int width)
+{
+	int i = 0;
+	for (i = 0; i < width; i++)
+	{
+		char tmp = *((char*)p1 + i);
+		*((char*)p1 + i) = *((char*)p2 + i);
+		*((char*)p2 + i) = tmp;
+	}
+}
+void bubble(void* base, int sz, int width, int(*cmp)(void*, void*))
+{
+	int i = 0;
+	for (i = 0; i < sz - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
+			{
+				swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[] = { 1,3,5,7,9,2,4,6,8,0 };
+	int i = 0;
+	bubble(arr, sizeof(arr) / sizeof(arr[0]), sizeof(int), cmp);
+	for (i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	return 0;
+}
