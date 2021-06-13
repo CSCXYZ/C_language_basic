d0b86c0b11e3076e07a9250e2040bf50304b7585
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-13 22:31:11 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2382,3 +2382,26 @@ int main()
 	return 0;
 }
  
+
+
+#include<stdio.h>
+//喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水
+int main()
+{
+	int money = 0;
+	int total = 0;
+	int empty = 0;
+	scanf("%d", &money);
+	//买回来的汽水喝掉
+	total = money;
+	empty = money;
+	//换回来的汽水
+	while (empty >= 2)
+	{
+		total += empty / 2;
+		empty = empty / 2 + empty % 2;
+	}
+	printf("%d ",total);
+	return 0;
+}
+ 
