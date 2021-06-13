aa8810ef7fe20efa1ac5b0db3a02bc3bc5980b0b
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-13 13:59:29 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2302,3 +2302,38 @@ int main()
 	return 0;
 }
  
+
+
+
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int i = 0;
+	for (i = 0; i <= 100000; i++)
+	{
+		//判断i是否为自幂数(水仙花数)
+		//1.计算i的位数 -n位数
+		int n = 1;
+		int tmp = i;
+		int sum = 0;
+		while (tmp /= 10)
+		{
+			n++;
+		}
+		//2.计算i的每一位的n次方之和sum
+		tmp = i;
+		while (tmp)
+		{
+			sum += pow(tmp % 10, n);
+			tmp /= 10;
+		}
+		//3.比较i==sum
+		if (i == sum)
+		{
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
+ 
