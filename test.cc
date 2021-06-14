cfbce8fa311cd8b18cf9d6097da3b67463ca2449
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-14 12:01:26 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2426,3 +2426,49 @@ int main()
 	return 0;
 }
  
+
+
+#include<stdio.h>
+//调整奇数偶数顺序
+//调整数组使奇数全部都位于偶数前面
+void move(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		//从左边找偶数
+		while ((left < right) && (arr[left] % 2 == 1))
+		{
+			left++;
+		}
+		//从右边找奇数
+		while ((left < right) && (arr[right] % 2 == 0))
+		{
+			right--;
+		}
+		if (left < right)
+		{
+			int tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+		}
+	}
+}
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	move(arr,sz);
+	print(arr, sz);
+	return 0;
+}
