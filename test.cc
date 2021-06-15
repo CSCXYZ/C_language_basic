7c383a53d9e5185018e5681c232ca665628da5c8
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-15 14:36:33 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2487,3 +2487,21 @@ int main()
 	return 0;
 }
   
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	//-128-->127
+	char a[1000] = { 0 };
+	int i = 0;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	//-1 -2 ...-128 127 126 125 ...3 2 1 0 -1 -2...
+	printf("%d\n", strlen(a));//255 strlen 0之前的计数
+	return 0;
+}
+  
