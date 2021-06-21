fb1ba1a372fd54b441402ff6b0547e907edec2ed
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-21 11:54:07 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2656,3 +2656,39 @@ int main()
 return 0;
 } 
   
+
+
+//旋转字符串(三步翻转法)
+//abcdef
+//bafedc
+//cdefab
+void reverse(char *left,char*right)
+{
+	assert(left != NULL);
+	assert(right != NULL);
+	while (left<right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+void left_move(char* arr,int k)
+{
+	assert(arr);
+	int len = strlen(arr);
+	assert(k <= len);
+	reverse(arr,arr+k-1);//逆序左边
+	reverse(arr+k,arr+len-1);//逆序右边
+	reverse(arr,arr+len-1);//逆序整体
+}
+int main() 
+{
+	char arr[] = "abcdef";
+	left_move(arr, 2);
+	printf("%s\n", arr);
+return 0;
+} 
+  
