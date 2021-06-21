06ca8d02b6b0cf215ebd6ef4ea101bc208e5a5c1
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-21 22:45:26 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2692,3 +2692,96 @@ int main()
 return 0;
 } 
   
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+//判断一个数组是否由另外一个数组旋转得来
+void reverse(char* left, char* right)
+{
+	assert(left != NULL);
+	assert(right != NULL);
+	while (left < right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+void left_move(char* arr, int k)
+{
+	assert(arr);
+	int len = strlen(arr);
+	assert(k <= len);
+	reverse(arr, arr + k - 1);//逆序左边
+	reverse(arr + k, arr + len - 1);//逆序右边
+	reverse(arr, arr + len - 1);//逆序整体
+}
+int is_left_move(char* s1, char* s2)
+{
+	int len = strlen(s1);
+	int i = 0;
+	for (i = 0; i < len; i++)
+	{
+		left_move(s1, 1);
+		int ret = strcmp(s1, s2);
+		if (ret == 0)
+			return 1;
+	}
+	return 0;
+}
+int main() 
+{
+	char arr1[] = "abcdef";
+	char arr2[] = "cdefab";
+	int ret = is_left_move(arr1, arr2);
+	if (ret == 1)
+		printf("Yes\n");
+	else
+		printf("No\n");
+return 0;
+} 
+  
+
+
+
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+//判断一个数组是否由另外一个数组旋转得来追加(abcdefabcdef)
+int is_left_move(char* str1, char* str2)
+{
+	int len1 = strlen(str1);
+	int len2 = strlen(str2);
+	if (len1!= len2)
+		return 0;
+	//1.在str1字符串中追加一个str1字符串
+	//strcat(str1,str1);err
+	strncat(str1, str1, 6);
+	//2.判断str2指向的字符串是否是str1指向字符串的子串
+	//strstr-找子串的
+	char* ret=strstr(str1, str2);
+	if (ret == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		return 1; 
+	}
+}
+int main() 
+{
+	char arr1[30] = "abcdef";
+	char arr2[] = "cdefab";
+	int ret = is_left_move(arr1, arr2);
+	if (ret == 1)
+		printf("Yes\n");
+	else
+		printf("No\n");
+return 0;
+} 
+  
