b39cbc95ad913ebd1b66d5e30a11cf72ff190316
CSCXYZ <82224462+CSCXYZ@users.noreply.github.com>
2021-06-22 11:21:09 +0800

    Update test.cc

--- test.cc
+++ test.cc
@@ -2785,3 +2785,92 @@ int main()
 return 0;
 } 
   
+
+
+
+//二分查找（数组为二维递增）
+int FindNum(int arr[3][3], int k, int row, int col)
+{
+	int x = 0;
+	int y = col - 1;
+	while (x <= row - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			printf("下标是%d %d\n", x, y);
+			return 1;
+		}
+	}
+	return 0;
+}
+int main() 
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 7;
+	int ret = FindNum(arr, k, 3, 3);
+	if (ret == 1)
+	{
+		printf("找到了\n");
+	}
+	else
+	{
+		printf("找不到\n");
+	}
+return 0;
+} 
+  
+
+
+
+//返回型参数 （不在函数内打印）
+int FindNum(int arr[3][3], int k, int *px, int *py)
+{
+	int x = 0;
+	int y = *py - 1;
+	while (x <= *px - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px =x;
+			*py = y;
+			return 1;
+		}
+	}
+	return 0;
+}
+int main() 
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 7;
+	int x = 3;
+	int y = 3;
+	//返回型参数 （不在函数内打印）
+	int ret = FindNum(arr, k, &x, &y);
+	if (ret == 1)
+	{
+		printf("找到了\n");
+		printf("下标是：%d %d\n", x, y);
+	}
+	else
+	{
+		printf("找不到\n");
+	}
+return 0;
+} 
+  
